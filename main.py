25767f141b51c4763a52163065a800c34127baa5
madil002 <muabdul.adil@gmail.com>
2024-04-25 15:14:44 +0100

    Adjusted params

--- main.py
+++ main.py
@@ -17,15 +17,15 @@ def main(distance_matrix, cxpb, mutpb, pop_size):
     stats.register("min", np.min)
     stats.register("max", np.max)
 
-    pop, log = algorithms.eaSimple(pop, toolbox, cxpb=0.7, mutpb=0.2, ngen=100, stats=stats, halloffame=hof, verbose=True)
+    pop, log = algorithms.eaSimple(pop, toolbox, cxpb=cxpb, mutpb=mutpb, ngen=100, stats=stats, halloffame=hof, verbose=True)
 
     return pop, log, hof
 
 if __name__ == "__main__":
     distance_matrix = generate_tsp("TSPs/wi29.tsp")
-    cxpb = 0.894698890
-    mutpb = 0.0654135104
-    pop_size = 150
+    cxpb = 0.967546724
+    mutpb = 0.111150540
+    pop_size = 149
     final_pop, logbook, best = main(distance_matrix, cxpb, mutpb, pop_size)
     print("Best individual is:", best[0])
     print("Best fitness is:", best[0].fitness.values[0])
